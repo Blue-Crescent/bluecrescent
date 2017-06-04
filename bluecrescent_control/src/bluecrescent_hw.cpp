8174d176f52ecb5f4de1b8e237374860003afc08
ubuntu <ick40195+ros@gmail.com>
2017-06-04 21:17:33 +0900

    Draft 1

--- bluecrescent_control/src/bluecrescent_hw.cpp
+++ bluecrescent_control/src/bluecrescent_hw.cpp
@@ -1,148 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <termios.h>
+#include <signal.h>
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <ros/xmlrpc_manager.h>
 #include <angles/angles.h>
 #include <bluecrescent_control/bluecrescent_hw.h>
 #include <iostream> // for debug
 #include <math.h>
 
-#include <errno.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <linux/i2c-dev.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#define QUIT_CHAR 0x03 // ctrl+c
 
-char buf[10];
-int com_serial;
-int failcount;
- 
+#define lrotate(x) (x=(((0x80 & x) >> 0x7) | (x << 1)))
+#define rrotate(x) (x=(((0x01 & x) << 0x7) | (x >> 1)))
+#define A(num) (VSET | (step[num].nA & 0x2) | (step[num].A & 0x1)) 
+#define B(num) (VSET | (step[num].nB & 0x2) | (step[num].B & 0x1))
+#define printstep(num) printf("%x %x %x %x\r\n",0x1 & step[num].A,(0x2&step[num].nA)>>1,0x1&step[num].B,(0x2&step[num].nB)>>1)
 
-//////////
-// Init I2Cdevice
-//////////
-int i2c_init(char filename[40], int addr)
-    {
-        int file;
- 
-        if ((file = open(filename,O_RDWR)) < 0)
-        {
-            printf("Failed to open the bus.");
-            /* ERROR HANDLING; you can check errno to see what went wrong */
-        com_serial=0;
-            exit(1);
-            }
- 
-    if (ioctl(file,I2C_SLAVE,addr) < 0)
-        {
-            printf("Failed to acquire bus access and/or talk to slave.\n");
-            /* ERROR HANDLING; you can check errno to see what went wrong */
-        com_serial=0;
-            exit(1);
-            }
-    return file;
-    }
- 
- 
-//////////
-// Set pointer address
-//////////
-void i2c_set_pointer(int address,int value,int file)
-    {
-    /*
-    //printf("end: 0x%x%x\n", address,value);
-    if (i2c_smbus_write_byte_data(file, address, value)<0)
-        {
-        fprintf(stderr, "Warning - write failed\n");
-        } 
-    */
-    char buf[10];
-    buf[0]=address;
-    buf[1]=value;
- 
-    if (write(file, buf, 2) != 2)
-        {
-        fprintf(stderr, "Error setting pointer\n");
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
-        //printf("w_0x%0*x\n", 2, buf[0]);
-        //printf("w_0x%0*x\n", 2, buf[1]);
-        com_serial=1;
-        failcount=0;
-        }
- 
-    }
- 
-//////////
-// Read n bytes
-//////////
- 
-char * i2c_read(int add1, int add2, int nbytes,int file)
-    {
-    int n;
- 
-    i2c_set_pointer(add1,add2,file);
- 
-    if (read(file, buf, nbytes) != nbytes)
-        {
-        fprintf(stderr, "Error reading %i bytes\n",nbytes);
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
-        for (n=0;n<nbytes;n++)
-            {
-            //printf("r_0x%0*x\n", 2, buf[n]);
-            }
-        com_serial=1;
-        failcount=0;
-        return buf;
-        }
-    }
- 
-//////////
-// Write n bytes
-//////////
-void i2c_write(int add1,int add2,int nbytes,char value[10],int file)
-    {
-    int n;
-    unsigned char buf[10];
-    buf[0] = add1;
-    buf[1] = add2;
-    if (nbytes>=1) buf[2] = value[0];
-    if (nbytes>=2) buf[3] = value[1];
-    if (nbytes>=3) buf[4] = value[2];
-    if (nbytes>=4) buf[5] = value[3];
- 
-    if (write(file, buf, nbytes) != nbytes)
-        {
-        fprintf(stderr, "Error writing %i bytes\n",nbytes);
-        com_serial=0;
-        failcount++;
-        } 
-    else
-        {
-        for (n=0;n<(nbytes+2);n++)
-            {
-            //printf("w_0x%0*x\n", 2, buf[n]);
-            }
-        com_serial=1;
-        failcount=0;
-        }
-    }
+
+#define PI 3.141592653589
+#define PI_step 360
+// Steps for 1 period loop
+#define STEP_N (360/(18/2))
+// Degree of 1 step
+#define DSTEP 9
+
+// 8830 Register
+#define VSET (0x26 << 2)
+#define CONTROL 0x0
+#define FAULT 0x1
+
+struct termios CookedTermIos;
+struct termios RawTermIos;
+struct timespec ts;
+
+// DRV8830 drvreg Register for phase A,B
+typedef struct 
+{
+ unsigned char CONTROL_A;
+ unsigned char CONTROL_B;
+ unsigned char FAULT_A;
+ unsigned char FAULT_B;
+}drv8830reg;
+
+// motorstep step
+typedef struct
+{
+ unsigned char A;
+ unsigned char nA;
+ unsigned char B;
+ unsigned char nB;
+}motorstep;
+
+drv8830reg drvreg[4];
+
+//FULL STEP
+//motorstep step[4] = {
+//	//M0
+//	{0xcc,(0x33 << 1),0x66,0x33},
+//	//M1
+//	{0xcc,(0x33 << 1),0x66,0x33},
+//	//M2
+//	{0xcc,(0x33 << 1),0x66,0x33},
+//	//M3
+//	{0xCC,(0x33 << 1),0x66,0x33}
+//};
+//HALF STEP
+motorstep step[4] = {
+	//M0
+	{0xC1,(0x1C << 1),0x70,(0x07 << 1)},
+	//M1
+	{0xC1,(0x1C << 1),0x70,(0x07 << 1)},
+	//M2
+	{0xC1,(0x1C << 1),0x70,(0x07 << 1)},
+	//M3
+	{0xC1,(0x1C << 1),0x70,(0x07 << 1)}
+};
+
+int stepcnt[4] = {0,0,0,0};
+unsigned const char drv8830_addr[2] = {0x60,0x61};
+unsigned const char drv8830_addr_M1[2] = {0x62,0x63};
+int fd[2];
+
+
+void motor_release(){
+	printf("MOTOR RELEASE!\n");
+	wiringPiI2CWriteReg8(fd[0],CONTROL,0x18);
+	wiringPiI2CWriteReg8(fd[1],CONTROL,0x18);
+}
+void motor_lock(uint8_t num){
+	printf("MOTOR LOCKED!\n");
+	wiringPiI2CWriteReg8(fd[0],CONTROL,A(num));
+	wiringPiI2CWriteReg8(fd[1],CONTROL,B(num));
+}
+void cwstep(uint8_t num){
+	lrotate(step[num].A);
+	lrotate(step[num].nA);
+	lrotate(step[num].B);
+	lrotate(step[num].nB);
+	wiringPiI2CWriteReg8(fd[0],CONTROL,A(num));
+	wiringPiI2CWriteReg8(fd[1],CONTROL,B(num));
+	stepcnt[num]++;
+}
+void ccwstep(uint8_t num){
+	rrotate(step[num].A);
+	rrotate(step[num].nA);
+	rrotate(step[num].B);
+	rrotate(step[num].nB);
+	wiringPiI2CWriteReg8(fd[0],CONTROL,A(num));
+	wiringPiI2CWriteReg8(fd[1],CONTROL,B(num));
+	stepcnt[num]--;
+}
+// Replacement SIGINT handler
+void mySigIntHandler(int sig)
+{
+  printf("Motor Stopped!\n");
+  ROS_WARN("Shutdown request received. Motor Stopping. Driver Released!");
+}
 
 BlueCrescent::BlueCrescent()
 {
   // connect and register the joint state interface
+  signal(SIGINT, mySigIntHandler);
 
-  // Head
+  //fd[0] = wiringPiI2CSetup(drv8830_addr[0]);
+  //fd[1] = wiringPiI2CSetup(drv8830_addr[1]);
 
   hardware_interface::JointStateHandle state_handle_head_roll("head_roll", &head_pos_[ROLL], &head_vel_[ROLL], &head_eff_[ROLL]);
   jnt_state_interface.registerHandle(state_handle_head_roll);
@@ -168,25 +162,78 @@ void BlueCrescent::read(ros::Time time, ros::Duration period)
 
 void BlueCrescent::write(ros::Time time, ros::Duration period)
 {
-  char *buffer;
-  char buf[10];
-  int file;
-
-  file=i2c_init("/dev/i2c-1",0x38); //dev,slavei2caddr 
-  buf[0] = 0x41;
-  buf[1] = 0xFF;
-  i2c_write(0xE6,0x0E,2,buf,file); //this will write value 0x41FF to the address 0xE60E
-	       
-  buffer=(char *)i2c_read(0xE6,0x0E,4,file); //reading a 4 bytes value at the address 0xE60E
-	         
-  close(file);
- 
   // Real Robot functionality coding here...
   // below code is simulating real robot delay.	
-  //pos_[0] = pos_[0] + 0.01* (cmd_[0] - pos_[0]);
-  head_pos_[ROLL] = head_cmd_[ROLL];// + 0.01*(head_cmd_[ROLL] - head_pos_[ROLL]);
-  head_pos_[YAW] = head_cmd_[YAW];// + 0.01*(head_cmd_[YAW] - head_pos_[YAW]);
-  //ROS_DEBUG_STREAM("Debug:" << pos_[0] << cmd_[0]);
+
+  int head_step_cmd_[4];
+
+  head_step_cmd_[ROLL] =(int) (PI_step * head_cmd_[ROLL]/PI);
+  head_step_cmd_[YAW] =(int) (PI_step * head_cmd_[YAW]/PI);
+
+  if(stepcnt[ROLL]<head_step_cmd_[ROLL]){
+  	cwstep(ROLL);
+  }else if(stepcnt[ROLL]>head_step_cmd_[ROLL]){
+ 	ccwstep(ROLL);
+  }
+  if(stepcnt[YAW]<head_step_cmd_[YAW]){
+  	cwstep(YAW);
+  }else if(stepcnt[YAW]>head_step_cmd_[YAW]){
+ 	ccwstep(YAW);
+  }
+
+  printstep(ROLL);
+  printstep(YAW);
+
+  head_pos_[ROLL] =(int) stepcnt[ROLL] * PI / PI_step;
+  head_pos_[YAW] =(int) stepcnt[YAW] * PI / PI_step;
+
+//ROS_DEBUG_STREAM("Debug:" << pos_[0] << cmd_[0]);
   // Dump cmd_ from MoveIt!, current simulated real robot pos_.
-  printf("%lf,%lf\n",head_pos_[ROLL],head_cmd_[ROLL]);
+  printf("%lf,%lf,%d,%d\n",head_pos_[ROLL],head_cmd_[ROLL],stepcnt[ROLL],head_step_cmd_[ROLL]);
+  printf("%lf,%lf,%d,%d\n",head_pos_[YAW],head_cmd_[YAW],stepcnt[YAW],head_step_cmd_[YAW]);
+  
+  //head_pos_[ROLL] = head_cmd_[ROLL];// + 0.01*(head_cmd_[ROLL] - head_pos_[ROLL]);
+  //head_pos_[YAW] = head_cmd_[YAW];// + 0.01*(head_cmd_[YAW] - head_pos_[YAW]);
 }
+
+
+// int main(void){
+// 	int c;
+// 	ts.tv_sec = 5;
+// 	ts.tv_nsec = 0;
+// 
+//         /* WHO AM I */
+//         fd[0] = wiringPiI2CSetup(drv8830_addr[0]);
+//         fd[1] = wiringPiI2CSetup(drv8830_addr[1]);
+// 
+// 	// Ctrl+C Abort
+// 	if(SIG_ERR== signal(SIGINT,sigcatch))
+// 		return 1;
+// 	// Key Scan
+// 	tcgetattr(STDIN_FILENO, &CookedTermIos);
+// 	RawTermIos = CookedTermIos;
+// 	cfmakeraw(&RawTermIos);
+// 	tcsetattr(STDIN_FILENO, 0, &RawTermIos);
+// 
+// 	//printf( "Motor running during push");
+// 	//clock_nanosleep(CLOCK_REALTIME, 0, &ts, NULL);
+// 	//printf( " 8:up 2:down 5:lock 1:motor1 3:motor2 anynum:release char:exit");
+// 	
+// 	while((c = getchar()) != QUIT_CHAR) {
+// 	//if(isprint(c)) {
+// 		//	putchar(c);
+// 			switch(c){
+// 				case '8':
+// 					cwstep(0);
+// 					printstep(0);
+// 			}
+// 
+// 	//	} else {
+// 	//		printf("<%02X>", c);
+// 	//	}
+// 	}
+// 
+// 
+// 	tcsetattr(STDIN_FILENO, 0, &CookedTermIos);
+// 	return 0;
+// }
